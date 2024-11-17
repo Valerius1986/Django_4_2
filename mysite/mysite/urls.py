375387a07860569381edd5f893585ec6379acec4
Valery <valery@dentasoft.pl>
2024-11-17 21:15:31 +0300

    Add image in models

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     # http://127.0.0.1:8000/myapp/
     path('myapp/', include('myapp.urls', namespace='myapp'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
